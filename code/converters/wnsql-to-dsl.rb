4bb37eb3e9bb596430493cf4bf25bdc52e21a635
William de Beaumont <wbeaumont@ihmc.us>
2013-07-19 14:31:06 -0400

    add pointers to WN DSL; fix configure script for bash 3.2 (on Mac OS X)

--- code/converters/wnsql-to-dsl.rb
+++ code/converters/wnsql-to-dsl.rb
@@ -8,32 +8,63 @@ require 'word_net_sql'
 $pos2ss_type = Hash[*%w{noun n verb v adj a adv r}]
 $pos2trips = Hash[*%w{noun n verb v adj adj adv adv}]
 
+def write_pointer(out, pointer_name, target_synset_offset, target_ss_type, target_word_number)
+  begin
+    if (pointer_name == 'Pertains to noun / Derived from adjective')
+      pointer_name =
+	case (target_ss_type)
+	  when 'n'; 'Pertains to'
+	  when 'a','s'; 'Derived from'
+	  else raise "Bogus WN pointer #{pointer_name} #{target_ss_type}%08d" % target_synset_offset
+	end
+    end 
+    out.print("(> WN::#{pointer_name.gsub(/ /,'_')})")
+    if (target_word_number > 0)
+      target_sense_key =
+	WordNetSQL.query_first_value("SELECT sense_key FROM senses WHERE synset_offset=? AND ss_type=? AND word_number=?;", target_synset_offset, target_ss_type, target_word_number)
+      out.print(" WN::|#{target_sense_key}|")
+    else
+      out.print(" WN::%s%08d" % [target_ss_type, target_synset_offset])
+    end
+    out.puts(")")
+  rescue Exception => e
+    raise "Error writing DSL for pointer #{[pointer_name, target_synset_offset, target_ss_type, target_word_number].inspect}\n#{e.message}\n#{e.backtrace.join("\n")}\n"
+  end
+end
+
 def write_dsl_for_ss_type(out, ss_type)
   WordNetSQL.db.execute("SELECT synset_offset, gloss FROM synsets WHERE ss_type=?;", ss_type) { |ss_row|
     synset_offset, gloss = *ss_row
-    out.puts("  (concept WN::%s%08d" % [ss_type, synset_offset])
-    # TODO separate examples and definitions, add tags, provenance?
-    out.puts("    (definition (text #{gloss.inspect}))")
-#      WordNetSQL.db.execute("SELECT source_synset_offset, source_ss_type, pointer_name, target_synset_offset, target_ss_type FROM pointers NATURAL JOIN pointer_symbols WHERE source_ss_type=?;", ss_type) { |ptr_row|
-#        source_synset_offset, source_ss_type, pointer_name, target_synset_offset, target_ss_type = *ptr_row
-#	# TODO
-#      }
-    WordNetSQL.db.execute("SELECT sense_number, sense_key, lemma FROM senses WHERE synset_offset=? AND ss_type=?;", synset_offset, ss_type) { |sense_row|
-      sense_number, sense_key, lemma = *sense_row
-      lemma_symbol = lemma.gsub(/'/, '^')
-      word_symbols = lemma_symbol.split(/_/).collect { |ws|
-        ws = "|#{ws.upcase}|" unless (ws =~ /^[A-Za-z^][0-9A-Za-z^\.-]*$/)
-	ws
-      }.join(' ')
-      out.print <<EOP
+    begin
+      out.puts("  (concept WN::%s%08d" % [ss_type, synset_offset])
+      # TODO separate examples and definitions, add tags, provenance?
+      out.puts("    (definition (text #{gloss.inspect}))")
+      WordNetSQL.db.execute("SELECT pointer_name, target_synset_offset, target_ss_type, target_word_number FROM pointers NATURAL JOIN pointer_symbols WHERE source_synset_offset=? AND source_ss_type=? AND source_word_number=0;", synset_offset, ss_type) { |ptr_row|
+	out.print("    ")
+	write_pointer(out, *ptr_row)
+      }
+      WordNetSQL.db.execute("SELECT sense_number, word_number, sense_key, lemma FROM senses WHERE synset_offset=? AND ss_type=?;", synset_offset, ss_type) { |sense_row|
+	sense_number, word_number, sense_key, lemma = *sense_row
+	lemma_symbol = lemma.gsub(/'/, '^')
+	word_symbols = lemma_symbol.split(/_/).collect { |ws|
+	  ws = "|#{ws.upcase}|" unless (ws =~ /^[A-Za-z^][0-9A-Za-z^\.-]*$/)
+	  ws
+	}.join(' ')
+	out.print <<EOP
     (sense WN::|#{sense_key}|
       (alias WN::#{lemma_symbol}.#{ss_type}.#{sense_number})
       (word (#{word_symbols}))
-      )
 EOP
-    }
-    out.puts "    )"
-    out.puts
+	WordNetSQL.db.execute("SELECT pointer_name, target_synset_offset, target_ss_type, target_word_number FROM pointers NATURAL JOIN pointer_symbols WHERE source_synset_offset=? AND source_ss_type=? AND source_word_number=?;", synset_offset, ss_type, word_number) { |ptr_row|
+	  out.print("      ")
+	  write_pointer(out, *ptr_row)
+	}
+      }
+      out.puts "    )"
+      out.puts
+    rescue Exception => e
+      raise "Error writing DSL for WN::#{ss_type}%08d:\n#{e.message}\n#{e.backtrace.join("\n")}\n" % synset_offset
+    end
   }
 end
 
