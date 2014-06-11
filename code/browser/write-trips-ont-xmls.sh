30fee0406af26903161638abc01c6d060c22422c
William de Beaumont <wbeaumont@ihmc.us>
2014-06-11 12:12:09 -0400

    make write-trips-ont-xmls.sh more generic WRT lisp implementation; added Makefile to put both data and XSL in etc/ont-browser/ in order to avoid violating Firefox's same-directory same-origin policy

--- code/browser/write-trips-ont-xmls.sh
+++ code/browser/write-trips-ont-xmls.sh
@@ -2,14 +2,47 @@
 
 output_dir=$1
 
-export TRIPS_BASE=../../../..
-export CONFIGDIR=$TRIPS_BASE/src/config
-export LISP=`grep -P -o -e '(?<=^LISP = ).*' $CONFIGDIR/lisp/defs.mk`
+CONFIGDIR="$TRIPS_BASE/src/config"
+
+# get environment from lisp/defs.mk
+`awk 'BEGIN { print "export" } /^([A-Z_]+) = ([^[:space:]]+)$/ { print " " $1 "=" $3 }' <$CONFIGDIR/lisp/defs.mk`
+
+# someday I'd like these to be in lisp/defs.mk, but that day hasn't come yet
+BATCH=--batch
+LOAD=--load
+EVAL=--eval
+QUIT="--eval '(quit)'"
+
+case "$LISP_FLAVOR" in
+  sbcl)
+    BATCH="--noinform --noprint --disable-debugger"
+    QUIT="--eval '(sb-ext:quit)'"
+    ;;
+  ccl)
+    LOAD=-l
+    EVAL=-e
+    QUIT="-e '(quit)'"
+    ;;
+  cmucl)
+    BATCH=-batch
+    LOAD=-load
+    EVAL=-eval
+    QUIT="-eval '(quit)'"
+    ;;
+  allegro)
+    BATCH=-batch -q
+    LOAD=-L
+    EVAL=-e
+    QUIT="-e '(exit)'"
+    ;;
+  *)
+    echo "Warning: unknown lisp flavor $LISP_FLAVOR" >&2
+esac
+
+# run genericized lisp command
 $LISP \
-  --noinform \
-  --noprint \
-  --disable-debugger \
-  --load write-trips-ont-xmls.lisp \
-  --eval "(run \"$output_dir\")" \
-  --eval '(sb-ext:quit)'
+  $BATCH \
+  $LOAD write-trips-ont-xmls.lisp \
+  $EVAL "(run \"$output_dir\")" \
+  $QUIT
 
