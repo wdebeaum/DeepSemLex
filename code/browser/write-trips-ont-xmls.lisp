c02f35fa4b7818ef74d788961d3222afbda64216
William de Beaumont <wdebeaum@cs.rochester.edu>
2015-04-17 11:22:20 -0400

    improve error reporting for ontology browser

--- code/browser/write-trips-ont-xmls.lisp
+++ code/browser/write-trips-ont-xmls.lisp
@@ -13,34 +13,39 @@
 	(ont-pkg (find-package :ont)))
     (maphash
       (lambda (concept-name concept)
-	(when (and (eq ont-pkg (symbol-package concept-name)) ;; ONT::*
-		   ;; was actually defined in TRIPS
-		   (provenance concept)
-		   (eq 'TRIPS (name (car (provenance concept)))))
-	  (with-open-file
-	    (
-	      xml
-	      (format nil "~a/ONT::~a.xml" output-dir
-		      (string-downcase (symbol-name concept-name)))
-	      :direction :output
-	      :if-exists :supersede
-	    )
-	    (format xml "<?xml version=\"1.0\"?>~&<?xml-stylesheet type=\"text/xsl\" href=\"ont-type.xsl\"?>~&<dsl>")
-	    ;; set and write the provenance so we don't keep repeating it
-	    (let ((*current-provenance* (car (provenance concept))))
-	      (dsl-to-xml-stream (listify *current-provenance*) xml)
-	      ;; write the main concept,
-	      (dsl-to-xml-stream (listify concept) xml)
-	      ;; ... its ancestors, 
-	      (dolist (anc (eval-path-expression '(+ >inherit) (list concept)))
-		(dsl-to-xml-stream (listify anc) xml))
-	      ;; ... and its children (including senses)
-	      (dolist (child (eval-path-expression '<inherit (list concept)))
-	        ;; TODO include non-parametric template definitions?
-		(dsl-to-xml-stream (listify child) xml))
-	      (format xml "~&</dsl>~%")
+        (handler-case
+	  (when (and (eq ont-pkg (symbol-package concept-name)) ;; ONT::*
+		     ;; was actually defined in TRIPS
+		     (provenance concept)
+		     (eq 'TRIPS (name (car (provenance concept)))))
+	    (with-open-file
+	      (
+		xml
+		(format nil "~a/ONT::~a.xml" output-dir
+			(string-downcase (symbol-name concept-name)))
+		:direction :output
+		:if-exists :supersede
 	      )
-	    )))
+	      (format xml "<?xml version=\"1.0\"?>~&<?xml-stylesheet type=\"text/xsl\" href=\"ont-type.xsl\"?>~&<dsl>")
+	      ;; set and write the provenance so we don't keep repeating it
+	      (let ((*current-provenance* (car (provenance concept))))
+		(dsl-to-xml-stream (listify *current-provenance*) xml)
+		;; write the main concept,
+		(dsl-to-xml-stream (listify concept) xml)
+		;; ... its ancestors,
+		(dolist (anc (eval-path-expression
+				 '(+ >inherit) (list concept)))
+		  (dsl-to-xml-stream (listify anc) xml))
+		;; ... and its children (including senses)
+		(dolist (child (eval-path-expression '<inherit (list concept)))
+		  ;; TODO include non-parametric template definitions?
+		  (dsl-to-xml-stream (listify child) xml))
+		(format xml "~&</dsl>~%")
+		)
+	      ))
+	    (error (e)
+	      (format *error-output* "Error while processing concept named ~s:~%~a~%" concept-name e))
+	    ))
       (concepts *db*)
       )
     ;; TODO W::*.xml
