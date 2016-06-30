6b032116fd2d15eea74439044a102fcd5f1bcd1d
William de Beaumont <wdebeaum@cs.rochester.edu>
2016-06-30 10:30:37 -0400

    nightly automatic lex/ont update

--- trips/src/LexiconManager/Data/new/cholelithiasis.lisp
+++ trips/src/LexiconManager/Data/new/cholelithiasis.lisp
@@ -8,10 +8,10 @@
   (senses;;;;; names of medical conditions/symptoms that are mass nouns
    ;;;;; i.e., they can't take an indefinite article (*an arthritis) and they have no plural form
    ((meta-data :wn ("cholelithiasis%1:26:00"))
-    (LF-PARENT ONT::cholelithiasis
+    (LF-PARENT ONT::cholelithiasis)
     (TEMPL MASS-PRED-TEMPL)
     (syntax (W::morph (:forms (-none))))
     )
    )
 )
-)))
+))
--- trips/src/LexiconManager/Data/new/insanity.lisp
+++ trips/src/LexiconManager/Data/new/insanity.lisp
@@ -8,11 +8,11 @@
   (senses;;;;; names of medical conditions/symptoms that are mass nouns
    ;;;;; i.e., they can't take an indefinite article (*an arthritis) and they have no plural form
    ((meta-data :wn ("insanity%1:26:00"))
-    (LF-PARENT ONT::insanity
+    (LF-PARENT ONT::insanity)
     (TEMPL MASS-PRED-TEMPL)
     (syntax (W::morph (:forms (-none))))
     )
    )
 )
-)))
+))
 
